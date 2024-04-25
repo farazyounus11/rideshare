dc3f62408531cbd4639f385047c335c5d8eea345
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-04-25 19:46:23 -0400

    uber.py

--- uber.py
+++ uber.py
@@ -1,76 +1,59 @@
 import streamlit as st
 import pandas as pd
+import streamlit_pandas as sp
 import pydeck as pdk
 import os
 
-st.title("NYC RideShare Visualization")
-st.header('By Faraz Younus | M.S. Stats & Data Science', divider='gray')
-st.header('Select month for Visualization!')
+@st.cache
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    df['Date'] = pd.to_datetime(df['Date/Time']).dt.date
+    df['Hour'] = pd.to_datetime(df['Date/Time']).dt.hour
+    df.drop(columns=['Date/Time'], inplace=True)
+    return df
 
-files = os.listdir('.')
-csv_files = [file for file in files if file.endswith('.csv')]
+file_options = ["April.csv", "August.csv", "July.csv", "June.csv","May.csv","September.csv"]
+selected_file = st.selectbox("Select a file", file_options)
+df = load_data(selected_file)
+create_data = {"Date": "select",
+                "Hour": "multiselect",
+              "Base": "select"}
 
-if not csv_files:
-    st.write("No CSV files found in the current directory.")
-else:
-    selected_file = st.selectbox('Select A Month', csv_files)
-    
-    if st.button('Select'):
-        st.session_state['selected_file'] = selected_file  
-if 'selected_file' in st.session_state:
-    # Reading the selected CSV file from session state
-    df = pd.read_csv(st.session_state['selected_file'])
-    df['Date/Time'] = pd.to_datetime(df['Date/Time'])
+all_widgets = sp.create_widgets(df, create_data, ignore_columns=["lat", "lon"])
 
-    # Assuming 'Base' is a column in your DataFrame
-    # Get unique bases and allow the user to select
-    base_options = df['Base'].unique().tolist()
-    selected_bases = st.multiselect('Choose Car Type', base_options, default=base_options)
+res = sp.filter_df(df, all_widgets)
+st.title("Streamlit AutoPandas")
+st.header("Original DataFrame")
+st.write(df)
 
-    if selected_bases:  # Check if any base is selected
-        df = df[df['Base'].isin(selected_bases)]
+st.header("Result DataFrame")
+st.write(res)
 
-    hour_range = st.slider(
-        "Select Hour of Day",
-        0, 24, (0, 24),
-        step=1,  # Hour steps
-        format="%d hours"  # Display format
-    )
-
-    # Further filtering DataFrame based on selected hour range
-    df['Hour'] = df['Date/Time'].dt.hour
-    df = df[df['Hour'].between(hour_range[0], hour_range[1], inclusive='both')]
-    base_count = df['Base'].value_counts(normalize=True) * 100  # normalize=True gives the relative frequencies
-    percentagedf = pd.DataFrame(base_count).reset_index()
-    percentagedf.columns = ['Base', 'Percentage']
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.write("This is the Data!")
-        st.write(df)
-
-    with col2:
-        st.write("Percentage Distribution of Ride Type")
-        st.write(percentagedf)
-
-
-    st.header('Map', divider='gray')
-    layer = pdk.Layer(
-        "ScatterplotLayer",
-        df,
-        get_position=["Lon", "Lat"],
-        get_color=[255, 255, 0],  # Yellow color for visibility
-        get_radius=12,
-    )
-    
-    # Define the PyDeck view state for initial map focus
-    view_state = pdk.ViewState(latitude=df['Lat'].mean(), longitude=df['Lon'].mean(), zoom=10)
-    
-    # Create the PyDeck chart
-    map = pdk.Deck(
-        layers=[layer],
-        initial_view_state=view_state,
-    )
-    
-    # Display the map in Streamlit
-    st.pydeck_chart(map)
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=40.7128,  # NYC latitude
+        longitude=-74.0060,  # NYC longitude
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=res,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=res,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
